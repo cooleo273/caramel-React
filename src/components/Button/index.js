19361548081343f49f6debb6c3f61360c853642f
Leo <leulteferi273@gmail.com>
2020-07-28 08:08:30 -0700

    added screens and components

---
+++ src/components/Button/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import "./styles.css"
+
+export default function Button() {
+    return (
+        <div>
+            <button>I am everywhere button</button>
+        </div>
+    )
+}
---
+++ src/screens/Home/index.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import Button from '../../components/Button'
+import "./styles.css"
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h1>I am home</h1>
+                <Button />
+            </div>
+        )
+    }
+}
