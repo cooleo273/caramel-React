6bd608a3df44e9763c648cd1c1d5f78cf2a24747
Leo <leulteferi273@gmail.com>
2020-07-29 02:37:37 -0700

    modified the home index

--- src/screens/Home/index.js
+++ src/screens/Home/index.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import Button from '../../components/Button'
+import Image from '../../components/Image'
 import "./styles.css"
+
 
 export default class Home extends Component {
     render() {
@@ -8,6 +10,7 @@ export default class Home extends Component {
             <div>
                 <h1>I am home</h1>
                 <Button />
+                <Image />
             </div>
         )
     }
